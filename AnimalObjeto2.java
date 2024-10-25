dcbe941cbeb661c9ba24f6c499453a1030f7a4de
amarallorena <159277845+amarallorena@users.noreply.github.com>
2024-10-25 15:33:36 -0300

    poo

---
+++ AnimalObjeto2.java
@@ -0,0 +1,126 @@
+
+		//Objeto 5 
+		animal5.setespecie("Coelho");
+		animal5.settamanho("Grande Porte");
+		animal5.setgenero("Fêmea");
+		animal5.setgrupo("Mamífero");
+		animal5.setsons("chia");
+		animal5.setacao("pula");
+		animal5.setgera("Filhotes");
+		animal5.setataque("Morde");
+
+		//Objeto 6 
+		animal6.setespecie("Coelho Filhote");
+		animal6.settamanho("Pequeno Porte");
+		animal6.setgenero("Macho");
+		animal6.setgrupo("Mamífero");
+		animal6.setsons("Chia");
+		animal6.setacao("Pula");
+		animal6.setgera("Filhote");
+		animal6.setataque("Morde");
+
+		//Objeto 7
+		animal7.setespecie("Galinha");
+		animal7.settamanho("Grande Porte");
+		animal7.setgenero("Fêmea");
+		animal7.setgrupo("Ovíparo");
+		animal7.setsons("Cacareja");
+		animal7.setacao("Cisca");
+		animal7.setgera("Pintinho e ovo");
+		animal7.setataque("Avança");
+
+		//Objeto 8
+		animal8.setespecie("Pintinho");
+		animal8.settamanho("Pequeno Porte");
+		animal8.setgenero("Macho");
+		animal8.setgrupo("Ovíparo");
+		animal8.setsons("Pia");
+		animal8.setacao("Cisca");
+		animal8.setgera("Ovo e pintinhos novos");
+		animal8.setataque("Defesa da Mãe");
+
+//Apresentando Objetos
+		
+		System.out.println("----- OBJETO 1 -----");
+		System.out.println(animal1.getespecie());
+		System.out.println(animal1.gettamanho());
+		System.out.println(animal1.getgenero());
+		System.out.println(animal1.getgrupo());
+		System.out.println(animal1.getsons());
+		System.out.println(animal1.getacao());
+		System.out.println(animal1.getgera());
+		System.out.println(animal1.getataque());
+		
+		System.out.println("----- OBJETO 2 -----");
+		System.out.println(animal2.getespecie());
+		System.out.println(animal2.gettamanho());
+		System.out.println(animal2.getgenero());
+		System.out.println(animal2.getgrupo());
+		System.out.println(animal2.getsons());
+		System.out.println(animal2.getacao());
+		System.out.println(animal2.getgera());
+		System.out.println(animal2.getataque());
+		
+		System.out.println("----- OBJETO 3 -----");
+		System.out.println(animal3.getespecie());
+		System.out.println(animal3.gettamanho());
+		System.out.println(animal3.getgenero());
+		System.out.println(animal3.getgrupo());
+		System.out.println(animal3.getsons());
+		System.out.println(animal3.getacao());
+		System.out.println(animal3.getgera());
+		System.out.println(animal3.getataque());
+		
+		System.out.println("----- OBJETO 4 -----");
+		System.out.println(animal4.getespecie());
+		System.out.println(animal4.gettamanho());
+		System.out.println(animal4.getgenero());
+		System.out.println(animal4.getgrupo());
+		System.out.println(animal4.getsons());
+		System.out.println(animal4.getacao());
+		System.out.println(animal4.getgera());
+		System.out.println(animal4.getataque());
+		
+		System.out.println("----- OBJETO 5 -----");
+		System.out.println(animal5.getespecie());
+		System.out.println(animal5.gettamanho());
+		System.out.println(animal5.getgenero());
+		System.out.println(animal5.getgrupo());
+		System.out.println(animal5.getsons());
+		System.out.println(animal5.getacao());
+		System.out.println(animal5.getgera());
+		System.out.println(animal5.getataque());
+		
+		System.out.println("----- OBJETO 6 -----");
+		System.out.println(animal6.getespecie());
+		System.out.println(animal6.gettamanho());
+		System.out.println(animal6.getgenero());
+		System.out.println(animal6.getgrupo());
+		System.out.println(animal6.getsons());
+		System.out.println(animal6.getacao());
+		System.out.println(animal6.getgera());
+		System.out.println(animal6.getataque());
+		
+		System.out.println("----- OBJETO 7 -----");
+		System.out.println(animal7.getespecie());
+		System.out.println(animal7.gettamanho());
+		System.out.println(animal7.getgenero());
+		System.out.println(animal7.getgrupo());
+		System.out.println(animal7.getsons());
+		System.out.println(animal7.getacao());
+		System.out.println(animal7.getgera());
+		System.out.println(animal7.getataque());
+		
+		System.out.println("----- OBJETO 8 -----");
+		System.out.println(animal8.getespecie());
+		System.out.println(animal8.gettamanho());
+		System.out.println(animal8.getgenero());
+		System.out.println(animal8.getgrupo());
+		System.out.println(animal8.getsons());
+		System.out.println(animal8.getacao());
+		System.out.println(animal8.getgera());
+		System.out.println(animal8.getataque());
+		
+	}
+
+}
